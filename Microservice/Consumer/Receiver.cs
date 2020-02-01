9d131b98dbff727595d37ee6c6ce40a9ef210d72
MahbubAhmed <ahmedmahbub35@gmail.com>
2020-02-01 12:14:44 +0600

    sample Micro Service

---
+++ Microservice/Consumer/Receiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("test", false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received ---> {0}",message);
+                };
+
+                channel.BasicConsume("test", true, consumer);
+
+                Console.WriteLine("press [enter] to exist the sender app--");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Microservice/Producer/Sender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("test", false, false, false, null);
+
+                string message = "hello world rabbit mq test";
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("","test", null, body);
+            }
+
+            Console.WriteLine("press [enter] to exist the sender app--");
+            Console.ReadLine();
+        }
+    }
+}
